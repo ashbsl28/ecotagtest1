d7d0828a0825db0da38a8c3eabead2d0ec4351b7
Aashish Bansal <ash@Aashishs-MacBook-Air.local>
2023-08-17 17:55:22 +0100

    Initial commit of React app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Menu from './components/Menu';
+
+const menuData = [
+    { id: 1, name: 'Artichoke', price: 10.99, img: 'path_to_image.jpg' },
+    { id: 2, name: 'Bacon', price: 8.99, img: 'path_to_image.jpg' },
+    { id: 3, name: 'Cucumber', price: 7.99, img: 'path_to_image.jpg' },
+    // ... add more items as needed
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Bruncherie Menu</h1>
+            </header>
+            <Menu items={menuData} />
+        </div>
+    );
 }
 
 export default App;
